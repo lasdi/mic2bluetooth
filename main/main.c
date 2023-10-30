db602bd1641ad6ea5c84f25248ad6a2544d491de
= <f7lipe@me.com>
2023-10-30 14:22:42 -0300

    removing unused methods and comments

--- main/main.c
+++ main/main.c
@@ -1,93 +1,14 @@
-#include "esp_log.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
 #include "src/hardware/bluetooth/bluetooth.h"
 #include "src/hardware/microphone/mic.h"
 #include "src/utils/audio_utils.h"
-#include "src/dsp/signal.h"
-#include <time.h>
-
-struct TaskInfo
-{
-    const char *name;
-    void (*task)();
-    double execution_time;
-};
-
-#define N_WINDOW_ELEMENTS 1024
-int signalVelocity = 1;
-int16_t buffer[N_WINDOW_ELEMENTS];
-int16_t mockedBuffer[N_WINDOW_ELEMENTS];
-
-void generate_simulated_signal()
-{
-    for (int i = 0; i < N_WINDOW_ELEMENTS; i++)
-    {
-        mockedBuffer[i] = (int16_t)(rand() % 1024);
-    }
-}
-
-bool init(void)
-{
-    generate_simulated_signal();
-    return mic_init();
-}
-
-void deinit(void)
-{
-    // bluetooth_cleanup();
-}
-
-void read_data()
-{
-    mic_read_data(buffer, N_WINDOW_ELEMENTS);
-}
-
-void modify_velocity()
-{
-    signal_modify_velocity(mockedBuffer, N_WINDOW_ELEMENTS, signalVelocity);
-}
 
 void app_main(void)
+
 {
-
-    if (init())
+    const bool mic_inited = mic_init();
+    const bool bluetooth_inited = bluetooth_init();
+    if (mic_inited && bluetooth_inited)
     {
-        ESP_LOGI("MAIN", "Initialization successful");
-
-#define N_REPETITIONS 1
-        struct TaskInfo tasks[] = {
-            {"Leitura do Microfone", read_data, 0.0},
-            {"Modificação da Velocidade do Sinal", modify_velocity, 0.0}};
-
-        int num_tasks = sizeof(tasks) / sizeof(tasks[0]);
-
-        for (int i = 0; i < num_tasks; i++)
-        {
-            clock_t start_time, end_time;
-
-            start_time = clock();
-            for (int j = 0; j < N_REPETITIONS; j++)
-            {
-                tasks[i].task();
-            }
-            end_time = clock();
-
-            tasks[i].execution_time = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
-        }
-
-        printf("Tarefa\t\tTempo médio de execução\n");
-        for (int i = 0; i < num_tasks; i++)
-        {
-            printf("%s\t\t%.3f s\n", tasks[i].name, tasks[i].execution_time / N_REPETITIONS);
-        }
+        mic_read_data();
     }
-    else
-    {
-        ESP_LOGE("MAIN", "Initialization failed");
-        return;
-    }
-
-    deinit();
 }
